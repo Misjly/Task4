f875dfd2ff5a5b8d97c83c0ae3b322a36f2def10
Misjly <nikmisg@gmail.com>
2021-02-07 23:30:47 +0300

    initial commit for task4

---
+++ Task4/ConsoleMessageBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Task4.BL.Custom;
+using Task4.BL.Custom.Builders;
+using System;
+
+namespace Task4.ConsoleClient
+{
+    public class ConloseMessageBuilder : BackupFeatureBuilder
+    {
+        protected override void Building()
+        {
+            base.Building();
+            BildConsoleMessaging();
+        }
+
+        protected virtual void BildConsoleMessaging()
+        {
+            TaskStrategyFactory.ActionContainer.OnCompleted += new EventHandler<CustomLogicTaskContext>(
+            (sender, context) => { Console.WriteLine("completed"); });
+            TaskStrategyFactory.ActionContainer.OnFaulted += new EventHandler<CustomLogicTaskContext>(
+            (sender, context) => { Console.WriteLine($"faulted on while processing {context.DataItem}"); });
+            TaskStrategyFactory.ActionContainer.OnCancelled += new EventHandler<CustomLogicTaskContext>(
+            (sender, context) => { Console.WriteLine("cancelled"); });
+        }
+
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Task4.BL.Custom.DataContext;
+using Task4.BL.Custom.DataSourceProviders;
+using System.Data.Entity;
+using Task4.BL.Strategies;
+using Task4.BL.Custom;
+
+namespace Task4.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DbContext context = new SaleContext())
+            {
+                context.Database.CreateIfNotExists();
+            }
+
+            var builder = new ConloseMessageBuilder();
+
+            GenericProcessStrategy<CsvDTO, CustomLogicTaskContext> handler = builder.Build();
+
+            handler.DataSourceProvider = new SAXFileProvider();
+
+            try
+            {
+                handler.Start();
+                handler.Stop();
+            }
+            finally
+            {
+                handler.Dispose();
+            }
+        }
+    }
+}
